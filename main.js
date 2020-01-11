7b86f3d3ed6bccee3b0691e829e137ef90f64224
Megan Venetianer <megan.venetianer@gmail.com>
2020-01-10 17:19:05 -0700

    Items on checklist can be deleted by clicking delete button

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ var draftTaskList = document.querySelector(".task-area");
 var addTaskButton = document.querySelector(".add-task-btn");
 var taskItemInput = document.querySelector(".task-item-input"
 );
+var taskItemsContainer = document.querySelector(".task-area");
 
 addTaskButton.addEventListener('click', populateDraftTasks);
 taskItemInput.addEventListener('keyup', validateItemInput);
+taskItemsContainer.addEventListener('click', clearTaskItem);
 
 function validateItemInput() {
   if (taskItemInput.value === "") {
@@ -17,11 +19,17 @@ function validateItemInput() {
 
 function populateDraftTasks() {
   draftTaskList.insertAdjacentHTML('afterbegin', `<div class = "draft-task-list">
-    <img src="./assets/check-yo-self-icons/delete.svg" alt="">
+    <img class = "draft-delete-btn" src="./assets/check-yo-self-icons/delete.svg" alt="">
     <p>${taskItemInput.value}</p>
   </div>`);
   taskItemInput.value = "";
   addTaskButton.disabled = true;
+}
+
+function clearTaskItem(event) {
+  if(event.target.classList.contains('draft-delete-btn')) {
+  event.target.closest('.draft-task-list').remove();
+  }
 }
 
 
